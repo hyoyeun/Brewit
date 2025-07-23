9511693beae5c9596bc6d88ce27e01283f04ef2e
hyoyeun <141557384+hyoyeun@users.noreply.github.com>
2025-07-23 14:36:10 +0900

    [방효윤] 회원가입 완료 페이지 디자인 완료

--- src/pages/SignUpCompletePage.jsx
+++ src/pages/SignUpCompletePage.jsx
@@ -1,8 +1,28 @@
 function SignUpCompletePage() {
     return (
-        <>
-            <p>회원가입 완료 페이지</p>
-        </>
+        <div className="flex flex-col justify-center items-center w-full h-fit">
+
+            {/* 안내문구 */}
+            <div className="flex flex-col justify-center items-center w-full h-auto my-[53px] p-[16px] max-w-96">
+                <div className="flex flex-col items-start  w-full h-auto  text-c-b gap-4">
+                    <div className="flex flex-col">
+                        <p className="font-[500] text-2xl">
+                            안녕하세요 <br/>
+                            노빌더님!
+                        </p>
+                    </div>
+                    <div className="flex justify-start">
+                        <p className="font-[500] text-xs  text-c-b6">회원가입이 완료되었습니다.</p>
+                    </div>
+                </div>
+
+
+                {/* 로그인하기 버튼 */}
+                <div className="flex flex-col mt-[240px]">
+                    <button>로그인하기</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
