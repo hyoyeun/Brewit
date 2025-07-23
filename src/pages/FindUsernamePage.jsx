53a35b74af2507da33a98125be30ff0755e588b7
hyoyeun <141557384+hyoyeun@users.noreply.github.com>
2025-07-23 13:31:31 +0900

    [방효윤] 아이디 찾기 디자인 완료

--- src/pages/FindUsernamePage.jsx
+++ src/pages/FindUsernamePage.jsx
@@ -1,8 +1,40 @@
 function FindUsernamePage() {
     return (
-        <>
-            <p>아이디 찾기 페이지</p>
-        </>
+        <div className="flex flex-col justify-center items-center w-full h-fit">
+
+            {/* 안내문구 */}
+            <div className="flex flex-col justify-center items-center w-full h-auto my-[53px] p-[16px] max-w-96">
+                <div className="flex flex-col items-start  w-full h-auto  text-c-b gap-4">
+                    <div className="flex flex-col">
+                        <p className="font-[500] text-2xl">
+                            아이디 찾기
+                        </p>
+                    </div>
+                    <div className="flex justify-start">
+                        <p className="font-[500] text-xs  text-c-b6">가입 시 입력한 성함과 메일 주소를 입력해주세요</p>
+                    </div>
+                </div>
+
+                {/* 성함 메일주소 입력창 */}
+                <div className="flex flex-col w-full h-auto justify-center items-start my-[74px] gap-[32px] text-c-b">
+                    <div>
+                        <p className="text-xs hidden">성함</p>
+                        <input type="text" placeholder="성함" className="w-[328px]" />
+                    </div>
+                    <div>
+                        <p className="text-xs hidden" >메일주소</p>
+                        <input type="email" placeholder="메일주소" className="w-[328px]" />
+                    </div>
+                </div>
+
+              
+
+                {/* 로그인 버튼 */}
+                <div className="flex flex-col mt-[240px]">
+                    <button>아이디 찾기</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
