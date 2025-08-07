f588720d93e8a46c1c9a2cd5e2f3e0d0f22505bd
hyoyeun <141557384+hyoyeun@users.noreply.github.com>
2025-08-07 17:02:41 +0900

    [방효윤] 마이페이지 레이아웃 구현 완료

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom';
+
 function MyPage() {
+
+    const [mypageOption, setMypageOption] = useState(["주문내역", "장바구니", "포인트", "약관", "계정정보", "설정"])
+    const [clicked, setClicked] = useState('')
+    const navigation = useNavigate();
     return (
-        <>
-            <p>마이 페이지</p>
-        </>
+        <div className={`flex flex-col w-full h-fit items-center gap-16 text-c-b pl-4 pr-4`}>
+            <div className="flex flex-row items-center min-w-[360px] h-fit w-[40vw] text-2xl font-medium justify-between mt-[57px]">
+                <p>{ }님 <br /> 환영합니다!</p>
+                <div className="w-5 h-5">
+                    <img src="/images/icon/notifications.svg" alt="" />
+                </div>
+            </div>
+            <div className="flex flex-row items-center min-w-[360px] h-fit w-[40vw] justify-between gap-2 flex-wrap">
+                {mypageOption.map((item, index) => (
+                    <div className={`flex flex-col justify-center items-center w-[30%] min-h-[84px] bg-white rounded-lg border-[1px] border-[rgba(19,19,19,0.1)] shadow-[0px_4px_24px_rgba(19,19,19,0.3)] `} key={index} >
+                        <div className="w-8 h-8">
+                            <img src={`images/icon/${index}.svg`} alt="icon" />
+                        </div>
+                        <div>
+                            <p className="font-semibold text-sm ">{item}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <p className="mt-[242px] font-medium text-xs text-c-b6">로그아웃</p>
+        </div>
+
     )
 }
 
